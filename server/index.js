819c06228eceef3b57bdd7d1b7beb15e5c8c343d
swagatikasahu123 <155278112+swagatikasahu123@users.noreply.github.com>
2025-07-19 18:51:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,18 +9,7 @@ const responseRoutes = require("./routes/responseRoutes");
 
 dotenv.config();
 const app = express();
-
-// ✅ Allow only specific frontend domains
-const allowedOrigins = [
-  "http://localhost:3000", // local frontend
-  "https://feedback-collection-plattform-m4fu.vercel.app" // deployed Vercel frontend
-];
-
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
@@ -28,7 +17,5 @@ app.use("/api/forms", formRoutes);
 app.use("/api/responses", responseRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
-  .then(() =>
-    app.listen(5000, () => console.log("✅ Backend running on http://localhost:5000"))
-  )
-  .catch(err => console.log("❌ DB Connection Failed:", err));
+    .then(() => app.listen(5000, () => console.log("✅ Backend running on http://localhost:5000")))
+    .catch(err => console.log("❌ DB Connection Failed:", err));
